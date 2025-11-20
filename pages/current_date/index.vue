c56159d36591d53c932814fb535e8422a9ea7760
HisashiHasebe <hhasebe@diverta.co.jp>
2025-11-20 10:31:47 +0800

    現在の時刻表示テスト

---
+++ pages/current_date/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const todayString = ref('')
+const nowString = ref('')
+
+onMounted(() => {
+  const today = new Date()
+  nowString.value = today.toString() // 表示用
+  today.setHours(0, 0, 0, 0)
+  todayString.value = today.toString() // 表示用
+})
+</script>
+
+<template>
+  <div style="padding: 20px;">
+    <h2>現在の時刻</h2>
+    <p>NOW: {{ nowString }}</p>
+    <p>0時設定後：{{ todayString }}</p>
+  </div>
+</template>
